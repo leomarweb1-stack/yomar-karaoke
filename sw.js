9fbd937773c9bd84aaba1f78184e8381bfa0151d
leomarweb1-stack <leomarweb1@gmail.com>
2026-06-24 18:10:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yomar-karaoke-v2';
+const CACHE_NAME = 'yomar-karaoke-v3';
 const APP_SHELL = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', function (event) {
